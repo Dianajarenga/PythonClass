9a91107e468fe88a72adf19597e04c56713fb3ec
Dianajarenga <jarengadiana@gmail.com>
2021-05-28 16:00:14 +0300

    classcar

---
+++ dog.py
@@ -0,0 +1,10 @@
+class Dod:
+    def __init__(self,age,breed,name):
+        self.age=age
+        self.breed=breed
+        self.name=name
+        
+
+    def digital_collar(self):
+        return f"I am a {self.age},{self.breed} , called {self.name}, "  
+   
